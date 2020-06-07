1724a2815fc2779537e443b0ec101662976eac6c
dbenitez-bcn <dbr.bcn@gmail.com>
2020-06-07 11:26:48 +0200

    Adds rounds to model

--- lib/model/workout_timer.dart
+++ lib/model/workout_timer.dart
@@ -3,8 +3,9 @@ import 'package:flutter/foundation.dart';
 class WorkoutTimer {
   int minutes;
   int seconds;
+  int round;
 
-  WorkoutTimer({@required this.minutes, @required this.seconds})
+  WorkoutTimer({@required this.minutes, @required this.seconds, int round = 5})
       : assert(minutes <= 99),
         assert(seconds < 60);
 }
