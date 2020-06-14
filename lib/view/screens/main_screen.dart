d5cf821798c5b03807f3914bf213b32c557c94db
dbenitez-bcn <dbr.bcn@gmail.com>
2020-06-14 12:47:30 +0200

    clean up code

--- lib/view/screens/main_screen.dart
+++ lib/view/screens/main_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:workout_timer/view/widgets/time_displayer.dart';
 import 'package:workout_timer/view/widgets/workout_button.dart';
 import 'package:workout_timer/viewmodel/timer_vm.dart';
 
@@ -16,16 +15,16 @@ class MainScreen extends StatelessWidget {
           return AnimatedOpacity(
             opacity: isDone ? 1 : 0,
             duration: Duration(milliseconds: 500),
-            child: StreamBuilder(
-              stream: this.vm.status,
-              initialData: Status.stopped,
-              builder: (context, snapshot) {
-                final appStatus = snapshot.data;
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Row(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  StreamBuilder<Status>(
+                    stream: this.vm.status,
+                    initialData: Status.stopped,
+                    builder: (context, snapshot) {
+                      final appStatus = snapshot.data;
+                      return Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: <Widget>[
                           WorkoutButtonEnable(
@@ -53,51 +52,11 @@ class MainScreen extends StatelessWidget {
                                 : null,
                           ),
                         ],
-                      ),
-                      StreamBuilder<String>(
-                        stream: this.vm.time,
-                        initialData: this.vm.getTime(),
-                        builder: (context, snapshot) {
-                          return TimerDisplayer(
-                            time: snapshot.data,
-                            round: this.vm.getRound(),
-                            percentage: this.vm.getPercentage(),
-                          );
-                        },
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          MaterialButton(
-                            color: Theme.of(context).primaryColor,
-                            child: Text("Stop"),
-                            onPressed: appStatus != Status.stopped
-                                ? () {
-                                    this.vm.stop();
-                                  }
-                                : null,
-                          ),
-                          SizedBox(
-                            width: 25,
-                          ),
-                          MaterialButton(
-                            color: Theme.of(context).primaryColor,
-                            child: Text(
-                                appStatus == Status.running ? "Pause" : "Play"),
-                            onPressed: () {
-                              if (appStatus == Status.running) {
-                                this.vm.pause();
-                              } else {
-                                this.vm.play();
-                              }
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
+                      );
+                    },
                   ),
-                );
-              },
+                ],
+              ),
             ),
           );
         },
--- lib/viewmodel/timer_vm.dart
+++ lib/viewmodel/timer_vm.dart
@@ -11,23 +11,16 @@ class TimerVM {
   WorkoutTimer _model;
   WorkoutTimer _modelSnapshot;
   Status _status;
-  String _lastTimeValue;
   AudioPlayer _audioPlugin;
 
   StreamController<Status> _statusController;
   Stream<Status> get status => _statusController.stream;
 
-  StreamController<String> _timeController;
-  Stream<String> get time => _timeController.stream;
-
   TimerVM() {
     this._modelSnapshot = WorkoutTimer(minutes: 0, seconds: 5);
     this._clearModel();
-    this._statusController = StreamController<Status>.broadcast();
-    this._timeController = StreamController<String>();
-    this._setStatus(Status.stopped);
+    this._statusController = StreamController<Status>();
     this._audioPlugin = AudioPlayer();
-    _run();
   }
 
   Future<void> load() async {
@@ -40,7 +33,6 @@ class TimerVM {
 
   void stop() {
     _clearModel();
-    _updateDisplayedTime(this._getSnapshotTime());
     this._setStatus(Status.stopped);
   }
 
@@ -55,7 +47,6 @@ class TimerVM {
   void setWorkoutTime(int minutes, int seconds) {
     this._modelSnapshot = WorkoutTimer(minutes: minutes, seconds: seconds);
     _clearModel();
-    _updateDisplayedTime(_getSnapshotTime());
   }
 
   String getTime() {
@@ -64,6 +55,14 @@ class TimerVM {
 
   String getRound() {
     return this._model.round < 0 ? "∞" : "${this._model.round}";
+  }
+
+  int getMinutes() {
+    return this._model.minutes;
+  }
+
+  int getSeconds() {
+    return this._model.seconds;
   }
 
   double getPercentage() {
@@ -77,35 +76,6 @@ class TimerVM {
     return (60 * workoutTimer.minutes) + workoutTimer.seconds;
   }
 
-  void _run() {
-    Timer.periodic(
-      Duration(seconds: 1),
-      (t) {
-        _updateDisplayedTime(_getDisplayTime());
-      },
-    );
-  }
-
-  String _getDisplayTime() {
-    switch (this._status) {
-      case Status.running:
-        return _getTimer();
-        break;
-      case Status.stopped:
-        return _getSnapshotTime();
-        break;
-      case Status.paused:
-        return getTime();
-        break;
-    }
-    return "Error";
-  }
-
-  String _getSnapshotTime() {
-    return _getFormatTime(
-        this._modelSnapshot.minutes, this._modelSnapshot.seconds);
-  }
-
   String _getFormatTime(int minutes, int seconds) {
     String time = "";
 
@@ -114,27 +84,6 @@ class TimerVM {
     time += _formatTime(seconds);
 
     return time;
-  }
-
-  String _getTimer() {
-    if (this._model.round != 0) {
-      if (this._model.minutes > 0 && this._model.seconds == 0) {
-        this._updateModel(this._model.minutes - 1, 59, this._model.round);
-      } else if (this._model.seconds > 0) {
-        this._updateModel(
-            this._model.minutes, this._model.seconds - 1, this._model.round);
-      } else {
-        this._updateModel(this._modelSnapshot.minutes,
-            this._modelSnapshot.seconds, this._model.round - 1);
-      }
-      if (this._model.minutes == 0 && this._model.seconds == 2) {
-        this._audioPlugin.play(this._goPath, isLocal: true);
-      }
-      return _getFormatTime(this._model.minutes, this._model.seconds);
-    } else {
-      this.stop();
-      return this._getSnapshotTime();
-    }
   }
 
   void _setStatus(Status newStatus) {
@@ -150,18 +99,6 @@ class TimerVM {
     return value < 10 ? "0$value" : "$value";
   }
 
-  void _updateModel(int minutes, int seconds, int round) {
-    this._model =
-        WorkoutTimer(minutes: minutes, seconds: seconds, round: round);
-  }
-
-  void _updateDisplayedTime(String value) {
-    if (value != this._lastTimeValue) {
-      this._lastTimeValue = value;
-      this._timeController.add(value);
-    }
-  }
-
   Future<void> _writeToFile(ByteData data, String path) async {
     final buffer = data.buffer;
     new File(path).writeAsBytes(
@@ -170,7 +107,6 @@ class TimerVM {
 
   void dispose() {
     this._statusController.close();
-    this._timeController.close();
   }
 }
 
